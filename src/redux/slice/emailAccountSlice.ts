0e8e3fb5498e4a7156c90937176f5940ec39e53a
Robindra <robindra.singha@impressico.com>
2025-02-24 15:44:42 +0530

    added code for redirection

---
+++ src/redux/slice/emailAccountSlice.ts
@@ -0,0 +1,38 @@
+import { createAsyncThunk} from "@reduxjs/toolkit";
+import {emailApi} from "../../services/api";
+import { AxiosError } from "axios";
+
+export interface EmailAccount {
+  id: string;
+  name: string;
+  email: string;
+  type: string;
+}
+
+export const fetchEmailAccount = createAsyncThunk(
+  "accounts",
+  async (_, { rejectWithValue }) => {
+  try {
+    const response = await emailApi.get("/accounts");
+    return response.data;
+  } catch (error: unknown) {
+    let errorMessage = "Something went wrong";
+    if (error instanceof AxiosError) {
+      errorMessage = (error.response?.data as string) || errorMessage;
+    }
+    return rejectWithValue(errorMessage);
+  }
+});
+
+export const addOuthEmailAccount = createAsyncThunk(
+  "oauth/auth-url",
+  async (_, { rejectWithValue }) => {
+    try {
+      const origin = encodeURIComponent(window.location.href);
+      const response = await emailApi.get(`/oauth/auth-url?origin=${origin}`);
+      return response.data.url;
+    } catch (error: any) {
+      return rejectWithValue(error.response?.data?.message || "Network error");
+    }
+  }
+);
