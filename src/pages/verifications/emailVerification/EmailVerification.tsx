ebe139dc148321476f2741155a3e598b373417a9
priyal-shapecast <priyal.garg@intelliatech.com>
2026-02-21 12:24:32 +0530

    Refactor EmailVerification component by removing tab navigation and associated state management for a simplified UI. Streamline layout by retaining only essential components for email verification.

--- src/pages/verifications/emailVerification/EmailVerification.tsx
+++ src/pages/verifications/emailVerification/EmailVerification.tsx
@@ -1,23 +1,11 @@
-import { useState } from "react";
-import { LeftPanel, PageContainer, RightPanel, Tab, TabsContainer, TabsList, VerificationContent } from "./EmailVerification.styled"
+import { LeftPanel, PageContainer, RightPanel, VerificationContent } from "./EmailVerification.styled"
 import StartVerificationOption from "./leftPanel/StartVerificationOption";
 import UploadFileOption from "./rightPanel/UploadFileOption";
 
 const EmailVerification = () => {
-  const [activeTab, setActiveTab] = useState<string>("verification");
   
   return (
     <PageContainer>
-      <TabsContainer>
-        <TabsList>
-          <Tab
-            $isActive={activeTab === "verification"}
-            onClick={() => setActiveTab("verification")}
-          >
-            Verify Email
-          </Tab>
-        </TabsList>
-      </TabsContainer>
       <VerificationContent>
         <LeftPanel><StartVerificationOption/> </LeftPanel>
         <RightPanel><UploadFileOption /></RightPanel>
